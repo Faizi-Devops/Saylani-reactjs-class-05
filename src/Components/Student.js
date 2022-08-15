33f0f9da66d35a6b3ee4e32bb8651a6ab01f40f4
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-15 01:38:54 -0700

    get data in console

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -3,19 +3,59 @@ import data from "./Data";
 
 let Student = () => {
     let [student, setStudent] = useState(data);
-    console.log(student);
+    let [kame,setKame]=useState();
+    let [katch,setKatch]=useState();
+    let [mole,setMole]=useState();
+    let [lass,setLass]=useState();
+
+    let one  = (e) =>{
+        setKame(e.target.value)
+
+    }
+    let two =(e) =>{
+        setKatch(e.target.value);
+
+    }
+    let three = (e) =>{
+        setMole(e.target.value)
+    }
+    let four  = (e)=> {
+        setLass(e.target.value)
+
+    }
+
+    let ongetData  = () =>{
+        let std = {
+            Name:kame,
+            Batch:katch,
+            Rollnumber:mole,
+            clas:lass
+        }
+        console.log(std)
+    }
+
+
+
+
+    
+
+    
+
     return (
         <div>
-            <input type="text" placeholder="Please enter the Name" /><br />
+            <h2>Add New Students</h2>
+            <input type="text" placeholder="Please enter the Name" onChange={one}/><br />
             <br />
-            <input type="text" placeholder="Please enter the Roll number"/><br />
+            <input type="text" placeholder="Please enter the Roll number" onChange={two} /><br />
             <br />
-            <input type="text"  placeholder="Pllease ente the Class Name"/><br />
+            <input type="text"  placeholder="Pllease ente the Class Name" onChange={three}/><br />
             <br />
-            <input type="text" placeholder="Please enter the Batch"/>
+            <input type="text" placeholder="Please enter the Batch" onChange={four}/>
 <br /><br />
-            <button type="button">Submit</button>
+            <button type="button" onClick={ongetData}>Submit</button>
             <br /><br />
+
+            <h2>List of Students</h2>
 
 
             <table class="table  table-striped">
